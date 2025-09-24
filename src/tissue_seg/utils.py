90aa217445e30ce1101f4d5bf11a3cf1837baa47
jmerta <julia-merta@wp.pl>
2025-09-24 19:23:44 +0200

    added small objects removal

--- src/tissue_seg/utils.py
+++ src/tissue_seg/utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 from skimage.color import rgb2hsv, rgb2lab
+from skimage import measure, morphology
 from scipy import ndimage as ndi
+from skimage.measure import label
+from sklearn.cluster import KMeans
+
 
 def get_strel_disk(radius):
     """
@@ -96,7 +100,32 @@ def apply_mask(img, mask, inv):
 def remove_small_objects(mask):
     """
     Remove objects with small area (smaller than 5% of image area)
+    :param mask: mask to remove objects
     :return: mask - mask with small objects removed
     """
-    mask = mask
+    thr_area = round(10 ** (0.45 * np.log10(mask.shape[0] * mask.shape[1])))
+
+    props = measure.regionprops(label(mask.astype(bool)))
+    areas = np.array([p.area for p in props])
+    area_tmp = areas[areas > thr_area]
+    kmeans = KMeans(n_clusters=2)
+    idx = kmeans.fit_predict(np.log10(area_tmp).reshape(-1, 1))
+    centers = kmeans.cluster_centers_.flatten()
+
+    if centers[0] > centers[1]:
+        thr_area = min(area_tmp[idx == 0]) - 1
+    else:
+        thr_area = min(area_tmp[idx == 1]) - 1
+
+
+    if np.sum(areas > thr_area) < 1:
+            kmeans = KMeans(n_clusters=2)
+            idx = kmeans.fit_predict(areas.reshape(-1, 1))
+            centers = kmeans.cluster_centers_.flatten()
+            if centers[0] > centers[1]:
+                thr_area = min(areas[idx == 0]) - 1
+            else:
+                thr_area = min(areas[idx == 1]) - 1
+
+    mask = morphology.remove_small_objects(mask.astype(bool), min_size=thr_area, connectivity=2)
     return mask
