0dda1148408452ff0bcbfc090a05ad393e13210d
jmerta <julia-merta@wp.pl>
2025-09-24 22:19:24 +0200

    Finished wsi_seg implementation (to test)

--- src/run_tissue_seg.py
+++ src/run_tissue_seg.py
@@ -1,13 +1,17 @@
 import os
 import argparse
+
+import cv2
+import numpy as np
 import pandas as pd
 import glob
 from openslide import OpenSlide
 import matplotlib.pyplot as plt
 import PIL.Image as Image
-import numpy as np
+from skimage import measure
 
-from src.tissue_seg.find_thr import get_pixel_distribution, GaMRed_hist
+from src.tissue_seg.tissue_seg import tissue_seg, plot_rgb_hist
+from src.tissue_seg.utils import apply_mask
 
 """
 Script for tissue region detection 
@@ -22,7 +26,6 @@ parser.add_argument('--fill_holes', type=bool, help='Fill holes in the tissue or
 parser.add_argument('--ask_for_overwrite', type=bool, help='Ask for overwriting the report file', default=False)
 args = parser.parse_args()
 
-
 MAG_BG_DET = 2.5 # magnification for tissue detection
 WSI_DIR = args.wsi_dir
 
@@ -30,18 +33,14 @@ WSI_DIR = args.wsi_dir
 BG_MASK_DIR = os.path.join(args.out_dir, 'background_masks')
 BG_THRESH_DIR = os.path.join(args.out_dir, 'background_thresholds')
 RAW_SMALL = os.path.join(args.out_dir, 'raw_small')
-PEN_CORRECTED = os.path.join(args.out_dir, 'pen_corrected')
 PEN_VIS = os.path.join(args.out_dir, 'pen_vis')
-GRAND_QC = os.path.join(args.out_dir, 'grand_qc')
-
+REMOVAL_VIS = os.path.join(args.out_dir, 'removal_vis')
 
 if not os.path.exists(BG_MASK_DIR): os.makedirs(BG_MASK_DIR)
 if not os.path.exists(BG_THRESH_DIR): os.makedirs(BG_THRESH_DIR)
 if not os.path.exists(RAW_SMALL): os.makedirs(RAW_SMALL)
-if not os.path.exists(PEN_CORRECTED): os.makedirs(PEN_CORRECTED)
 if not os.path.exists(PEN_VIS): os.makedirs(PEN_VIS)
-if not os.path.exists(GRAND_QC): os.makedirs(GRAND_QC)
-
+if not os.path.exists(REMOVAL_VIS): os.makedirs(REMOVAL_VIS)
 
 # Check if log file exists - ask if user want to overwrite it
 REPORT_FILE = os.path.join(args.out_dir, 'report_bg_removal.csv')
@@ -83,58 +82,42 @@ for slide in slides:
         scale_factor = MAG_BG_DET / mag
         region = region.resize((int(w * scale_factor), int(h * scale_factor)), Image.BICUBIC)
 
-    scale_thumbnail = 1/MAG_BG_DET
+
+    scale_thumbnail = 1/(MAG_BG_DET)
     thumbnail = region.resize((int(w * scale_thumbnail), int(h * scale_thumbnail)), Image.BICUBIC)
     thumbnail.save(os.path.join(RAW_SMALL, f'{basename}.png'))
 
-    # get the distribution of pixel values per color channel
-    img_np = np.array(region)
-    R, G, B = get_pixel_distribution(img_np)
+    region = region.convert('RGB')
+    res_dict = tissue_seg(region)
 
-    x = np.arange(256)
-    K= 2
-    SW = 5
-    draw = False
-
-    thr = np.zeros(3)
-    thr[0] = GaMRed_hist(x, R, K, draw, SW)[0]
-    thr[1] = GaMRed_hist(x, G, K, draw, SW)[0]
-    thr[2] = GaMRed_hist(x, B, K, draw, SW)[0]
-
-    bins = np.arange(-0.5, 255.5, 1)
-
-
-    bins = np.arange(0, 255, 1)
-    fig, axs = plt.subplots(3, 1, figsize=(8, 15))
-
-    axs[0].bar(bins, R, color='red')
-    axs[0].set_title('Red')
-    axs[1].bar(bins, G, color='green')
-    axs[1].set_title('Green')
-    axs[2].bar(bins, B, color='blue')
-    axs[2].set_title('Blue')
-
-    for ax in axs:
-        ax.set_xlim(0, 255)
-
-    axs[2].set_xlabel("Pixel value")
-    axs[1].set_ylabel("Count")
-
-    plt.tight_layout()
+    # save histograms with thresholds
+    fig, ax = plot_rgb_hist(res_dict['R'], res_dict['G'], res_dict['B'], res_dict['thr'])
     plt.show()
+    fig.savefig(os.path.join(BG_THRESH_DIR, f'{basename}_thr.png'))
+    plt.close(fig)
 
-    print(mag_layers)
+    # save marker removal effect results
+    mask_pen = Image.fromarray(apply_mask(np.array(region), res_dict['mask_pen'], inv=False))
+    mask_pen = mask_pen.resize((int(w * scale_thumbnail), int(h * scale_thumbnail)), Image.BICUBIC)
+    mask_pen.save(os.path.join(PEN_VIS, f'{basename}_pen.png'))
 
-    print(slide.level_dimensions)
+    # save mask for grandQC
+    np.save(os.path.join(BG_MASK_DIR, f'{basename}_mask.npy'), res_dict['mask'])
+    mask = Image.fromarray((res_dict['mask']*255).astype(np.uint8))
+    mask.save(os.path.join(BG_MASK_DIR, f'{basename}_mask.png'))
 
-    mag_idx = 3
+    # save mask visualisations on the tissue image
+    region = np.array(region)
+    vis_tissue = Image.fromarray(apply_mask(region, res_dict['mask'], inv=False))
+    vis_tissue = vis_tissue.resize((int(w * scale_thumbnail), int(h * scale_thumbnail)), Image.BICUBIC)
+    vis_tissue.save(os.path.join(REMOVAL_VIS, f'{basename}_thumbnail.png'))
 
-    # read region with chosen magnification
-
-    plt.figure(figsize=(20, 20))
-    plt.imshow(region)
-    plt.axis('off')
-    plt.show()
+    # save borders visualisation on the tissue image
+    contours, _ = cv2.findContours(res_dict['mask'].astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(region, contours, -1, (0, 0, 255), 2)
+    region = Image.fromarray(region)
+    region.resize((int(w * scale_thumbnail), int(h * scale_thumbnail)), Image.BICUBIC)
+    region.save(os.path.join(REMOVAL_VIS, f'{basename}_contour.png'))
 
     log_slide = {}
     if os.path.exists(REPORT_FILE):
