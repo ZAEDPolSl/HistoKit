d628baadec4bd442c45c4a606a7410b6c972e056
jmerta <julia-merta@wp.pl>
2025-09-25 22:48:24 +0200

    Added Grand QC for artifacts detection

--- src/run_tissue_seg.py
+++ src/run_tissue_seg.py
@@ -1,14 +1,20 @@
 import os
 import argparse
+import timeit
 import cv2
 import numpy as np
 import pandas as pd
 import glob
+import scipy.io
+import torch
 from openslide import OpenSlide
 import matplotlib.pyplot as plt
 import PIL.Image as Image
+from src.grand_qc.utils import slide_info, make_overlay
+from src.grand_qc.wsi_process import slide_process_single
 from src.tissue_seg.tissue_seg import wsi_tissue_seg, plot_rgb_hist
 from src.tissue_seg.utils import apply_mask
+from grand_qc.config import config
 
 """
 Script for tissue region detection 
@@ -18,12 +24,26 @@ Original matlab implementation is available in: github.com/WSI_TissueSeg
 parser = argparse.ArgumentParser()
 parser.add_argument('--wsi_dir', type=str, help='Input directory with WSIs', default='../test_data/wsi/')
 parser.add_argument('--out_dir', type=str, help='Output directory', default='../test_data/res/')
-parser.add_argument('--fill_holes', type=bool, help='Fill holes in the tissue or not', default=False)
+parser.add_argument('--fill_holes', type=bool, help='Fill holes in the tissue or not', default=True)
+parser.add_argument('--close_disk_r', type=int, help='Radius for disk strel used during mask cleaning with image closing', default=2)
+parser.add_argument('--open_disk_r', type=int, help='Radius for disk strel used during mask cleaning with image opening', default=2)
+parser.add_argument('--save_mask_formats', nargs='+', help='File formats to save masks, choose at least one from: npy, mat.', choices=["npy", "mat"], default=["npy", "mat"])
 parser.add_argument('--ask_for_overwrite', type=bool, help='Ask for overwriting the report file', default=False)
+parser.add_argument('--device', help='Device used for artifacts detection: cuda or cpu', choices=["cuda", "cpu"], default="cuda")
+parser.add_argument('--overlay_factor', help='Factor used for creating image overlay', default=0.60, type=float)
+parser.add_argument('--grandqc_model', help='Path to GrandQC model weights (model for 10x magnification is used by default).', default="grand_qc/models/GrandQC_MPP1.pth", type=str)
+parser.add_argument('--num_workers', help="Number of workers used to process images in parallel.", default=4, type=int, choices=range(1,os.cpu_count()+1))
 args = parser.parse_args()
 
 MAG_BG_DET = 2.5 # magnification for tissue detection
 WSI_DIR = args.wsi_dir
+M_P_S_MODEL = 512
+ENCODER_MODEL = 'timm-efficientnet-b0'
+ENCODER_MODEL_WEIGHTS = 'imagenet'
+BACK_CLASS = 7
+MPP_MODEL = 1
+
+model_prim = torch.load(args.grandqc_model, map_location=args.device)
 
 # Create folders for results
 BG_MASK_DIR = os.path.join(args.out_dir, 'background_masks')
@@ -66,6 +86,7 @@ for slide in slides:
     # load image with 2.5 magnification (if it is not available - rescale it from the highest magnification)
     mag = float(slide.properties["openslide.objective-power"])  # get magnification of the 0 layer
     mag_layers = [round(mag / r, 2) for r in ratio]  # magnification of each layer
+    mpp_slide = 10/mag_layers[0]
 
     if MAG_BG_DET in mag_layers:
         mag_idx = mag_layers.index(MAG_BG_DET)
@@ -84,7 +105,7 @@ for slide in slides:
     thumbnail.save(os.path.join(RAW_SMALL, f'{basename}.png'))
 
     region = region.convert('RGB')
-    res_dict = wsi_tissue_seg(region)
+    res_dict = wsi_tissue_seg(region, args.fill_holes, args.close_disk_r, args.open_disk_r)
 
     # save histograms with thresholds
     fig, ax = plot_rgb_hist(res_dict['R'], res_dict['G'], res_dict['B'], res_dict['thr'])
@@ -98,9 +119,15 @@ for slide in slides:
     mask_pen.save(os.path.join(PEN_VIS, f'{basename}_pen.png'))
 
     # save mask for grandQC
-    np.save(os.path.join(BG_MASK_DIR, f'{basename}_mask.npy'), res_dict['mask'])
+    if "npy" in args.save_mask_formats:
+        np.save(os.path.join(BG_MASK_DIR, f'{basename}_mask.npy'), res_dict['mask'])
+    if "mat" in args.save_mask_formats:
+        scipy.io.savemat(os.path.join(BG_MASK_DIR, f'{basename}_mask.mat'), res_dict, do_compression=True)
+
+    # save mask as color thumbnail
     mask = Image.fromarray((res_dict['mask']*255).astype(np.uint8))
-    mask.save(os.path.join(BG_MASK_DIR, f'{basename}_mask.png'))
+    mask = mask.resize((int(w * scale_thumbnail), int(h * scale_thumbnail)), Image.Resampling.NEAREST)
+    mask.save(os.path.join(BG_MASK_DIR, f'{basename}_mask_small.png'))
 
     # save mask visualisations on the tissue image
     region = np.array(region)
@@ -115,11 +142,42 @@ for slide in slides:
     region.resize((int(w * scale_thumbnail), int(h * scale_thumbnail)), Image.BICUBIC)
     region.save(os.path.join(REMOVAL_VIS, f'{basename}_contour.png'))
 
-    log_slide = {}
-    if os.path.exists(REPORT_FILE):
-        pd.DataFrame(log_slide).to_csv(REPORT_FILE, mode="a", header=False, index=False)
-    else:
-        pd.DataFrame(log_slide).to_csv(REPORT_FILE, mode="w", header=True, index=False)
+    ############################################################################################################
+    # RUN GRAND QC FOR ARTIFACTS DETECTION
+    ############################################################################################################
+
+    p_s, patch_n_w_l0, patch_n_h_l0, w_l0, h_l0, obj_power = slide_info(slide, M_P_S_MODEL, MPP_MODEL, mpp_slide)
+
+    h, w = res_dict["mask"].shape
+    tis_det = Image.fromarray(1-res_dict["mask"].astype(np.uint8))
+    tis_det = np.array(tis_det.resize((int(w*4), int(h*4)), Image.Resampling.NEAREST))
+
+    map_tiss, full_mask = slide_process_single(model_prim, tis_det, slide, patch_n_w_l0, patch_n_h_l0, p_s,
+                                               M_P_S_MODEL, config.colors, ENCODER_MODEL,
+                                               ENCODER_MODEL_WEIGHTS, args.device, BACK_CLASS, MPP_MODEL, mpp_slide, w_l0, h_l0,
+                                               (w, h))
+
+    stop = timeit.default_timer()
+    map_path = os.path.join(BG_MASK_DIR , basename + "_map_QC.png")
+    map_tiss.save(map_path)
+    mask_path = os.path.join(BG_MASK_DIR , basename + "_mask_qc.npy")
+    full_mask = Image.fromarray(full_mask)
+    full_mask = full_mask.resize((int(region.size[0]), int(region.size[1])) , Image.Resampling.NEAREST)
+    np.save(mask_path, np.array(full_mask))
+
+    del full_mask
+
+    map_tiss = Image.open(map_path)
+    overlay = make_overlay(region, map_tiss, args.overlay_factor)
+
+    del map_tiss
+
+    # Save overlaid image
+    overlay_im = Image.fromarray(overlay)
+    overlay_im_name = os.path.join(BG_MASK_DIR , basename + "_overlay_QC.jpg")
+    overlay_im.save(overlay_im_name)
+
+    del overlay
 
 
 
--- src/tissue_seg/tissue_seg.py
+++ src/tissue_seg/tissue_seg.py
@@ -4,7 +4,15 @@ from src.tissue_seg.find_thr import get_thr_image
 from src.tissue_seg.utils import apply_mask, remove_pen, remove_gray_stains, get_strel_disk, remove_small_objects
 from scipy import ndimage as ndi
 
-def wsi_tissue_seg(region, fill_holes=False):
+def wsi_tissue_seg(region, fill_holes=False, open_disk_r=2, close_disk_r=2):
+    """
+
+    :param region:
+    :param fill_holes:
+    :param open_disk_r:
+    :param close_disk_r:
+    :return:
+    """
     img_np = np.array(region)
 
     # get thresholds for each channel (GaMRed or Otsu when threshold is too low)
@@ -22,10 +30,17 @@ def wsi_tissue_seg(region, fill_holes=False):
     # remove gray stains with low Chroma component
     mask = remove_gray_stains(img_np, mask)
 
+    # fill holes in mask (if fill_holes==True)
+    if fill_holes:
+        mask = ndi.binary_fill_holes(mask)
+
     # morphological operations to clean the mask
-    SE = get_strel_disk(2)
-    mask = ndi.binary_closing(mask, SE)
-    mask = ndi.binary_opening(mask, SE)
+    SE_close = get_strel_disk(close_disk_r)
+    SE_open = get_strel_disk(open_disk_r)
+    mask = ndi.binary_closing(mask, SE_close)
+    mask = ndi.binary_opening(mask, SE_open)
+
+    # remove small regions
     mask = remove_small_objects(mask)
 
     return {"mask": mask, "mask_pen": mask_pen, "R": R, "G": G, "B": B, "thr":thr}
@@ -33,16 +48,17 @@ def wsi_tissue_seg(region, fill_holes=False):
 def plot_rgb_hist(R, G, B, thr):
     bins = np.arange(len(R))
 
-    fig, axs = plt.subplots(3, 1, figsize=(8, 6), sharex=True)
+    fig, axs = plt.subplots(3, 1, figsize=(6, 8), sharex=True)
 
     channels = [("R", R, "Red"),
                 ("G", G, "Green"),
                 ("B", B, "Blue")]
 
     for ax, (name, hist, color) in zip(axs, channels):
-        ax.bar(bins, hist, color=color)
-        ax.axvline(thr[name], color="black", linestyle="--", linewidth=1.5)
-        ax.set_title(f"{name} channel histogram")
+        ax.bar(bins, hist, color=color, width=1)
+        ax.set_yscale("log")
+        ax.axvline(thr[name], color="Orange", linestyle="--", linewidth=2)
+        ax.set_title(f"{name} channel histogram: thr = {thr[name]:.2f}")
 
     plt.tight_layout()
     return fig, axs
