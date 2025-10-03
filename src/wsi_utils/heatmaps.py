9d84bba3aa27126a548b0e923b42544d64db9703
jmerta <julia-merta@wp.pl>
2025-10-03 12:06:54 +0200

    Removed overlay factor

---
+++ src/wsi_utils/heatmaps.py
@@ -0,0 +1,14 @@
+
+def patch_wsi(wsi_path, patch_size, overlap, mirroring_type, mag):
+    """
+    Split WSI into patches.
+    :param wsi_path:
+    :param patch_size:
+    :param overlap:
+    :param mirroring_type:
+    :param mag: Desired slide magnification.
+    :return:
+    """
+
+
+
