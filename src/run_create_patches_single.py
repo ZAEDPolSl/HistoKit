ccd9059d035b70edc12efdfb4541a809834fa3b8
jmerta <julia-merta@wp.pl>
2025-10-21 12:59:59 +0200

    Fixed imports

--- src/run_create_patches_single.py
+++ src/run_create_patches_single.py
@@ -7,6 +7,8 @@ from openslide import OpenSlide
 from PIL import Image
 import scipy.io as sio
 
+from src.histo_kit.wsi_utils.patches import load_wsi_mag, read_region, patch_wsi
+
 """
 Script for dividing image into patches
 """
