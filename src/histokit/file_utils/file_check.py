9e4fab78e31a4bd6168e2aa294eee0c319e53385
Julia Merta <julia.merta@polsl.pl>
2026-07-08 09:29:02 +0200

    added file utils

---
+++ src/histokit/file_utils/file_check.py
@@ -0,0 +1,19 @@
+import os
+
+
+def check_gt_pred_exists(gt_path, pred_path):
+    return True if os.path.exists(gt_path) and os.path.exists(pred_path) else False
+
+def check_gt_pred_folders(gt_folder, pred_folder, use_ext = False):
+    
+    gt_files = os.listdir(gt_folder)
+    pred_files = os.listdir(pred_folder)
+
+    gt_files = [os.path.splitext(f)[0] for f in gt_files]
+    pred_files = [os.path.splitext(f)[0] for f in pred_files]
+
+    if not use_ext:
+        gt_files = [os.path.splitext(f)[0] for f in gt_files]
+        pred_files = [os.path.splitext(f)[0] for f in pred_files]
+
+    return set(gt_files) == set(pred_files)
